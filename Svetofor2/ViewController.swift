add31c1c39d9fe6a94224eb6fb8d44184f70f892
Daniil Kozlov <enjoy2k@mail.ru>
2023-06-01 13:17:59 +0300

    Initial Commit

---
+++ Svetofor2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Svetofor2
+//
+//  Created by Евгения Максимова on 01.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
